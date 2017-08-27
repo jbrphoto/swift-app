6e421b18a186320c6793bd8d4c8e02c69f827ac8
niksen <sierra@Sierras-MacBook-Pro.local>
2017-08-27 16:42:13 -0700

    8/27/17

--- Swift app/ViewController.swift
+++ Swift app/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Sierra on 6/18/17.
 //  Copyright © 2017 Sierra. All rights reserved.
-//
+//  
 
 import UIKit
 
@@ -12,10 +12,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var Label: UILabel!
     
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    
+    @IBAction func Button(_ sender: Any) {
+    
+    
+    
+  
+        print(text1)
+        print(text1.text!)
+        print(text2.text!)
+    
+}
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    Label.text = "Hello"
+        // Do any additional setup after loading the view, "
     }
 
     override func didReceiveMemoryWarning() {
